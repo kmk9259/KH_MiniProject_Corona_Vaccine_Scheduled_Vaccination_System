d66bbc7e41a2ed0174dd5f42d091c88ca28c82d5
Sunyoring <sunhyo67@gmail.com>
2021-06-03 20:49:06 +0900

    MainMenu

--- src/mini/kh1/corona/run/Run.java
+++ src/mini/kh1/corona/run/Run.java
@@ -1,10 +1,12 @@
 package mini.kh1.corona.run;
 
+import mini.kh1.corona.view.MainMenu;
+
 public class Run {
 
 	public static void main(String[] args) {
-		System.out.println("zz");
 
+		new MainMenu();
 	}
 
 }
