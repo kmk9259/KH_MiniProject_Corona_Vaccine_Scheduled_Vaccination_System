4cf92d5a2ff1998795d71e8abb72efd89a267e93
phant <phant@DESKTOP-A7I0ADU>
2021-06-03 19:02:39 +0900

    ㅣㅣ

---
+++ src/test/dd/juneTest.java
@@ -0,0 +1,10 @@
+package test.dd;
+
+public class juneTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("여기 테스트 해봅니다~~!!");
+		System.out.println("나나나");
+	}
+}
