e513f636d0fea8f18e0ec48373bcf70c10053f30
DESKTOP-A7I0ADU\phant <phantom1984meriel@gmail.com>
2021-06-08 14:54:03 +0900

    login gui

---
+++ src/mini/kh1/corona/view/InsertPage.java
@@ -0,0 +1,178 @@
+package mini.kh1.corona.view;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import mini.kh1.corona.controller.InsertConstroller;
+import mini.kh1.corona.model.vo.User;
+
+public class InsertPage extends JFrame implements ActionListener {
+	
+	
+	private User u = new User();
+	InsertConstroller ic = new InsertConstroller();
+
+	private JFrame frame = new JFrame();
+	private Container container = getContentPane();
+	private Scanner sc = new Scanner(System.in);
+
+	// 레이블
+	private JLabel userLabel = new JLabel("아이디");
+	private JLabel passwordLabel = new JLabel("비밀번호");
+	private JLabel nameLabel = new JLabel("이름");
+	private JLabel ssnLabel = new JLabel("주민 번호");
+	private JLabel emailLabel = new JLabel("이메일");
+
+	// 필드부
+	private JTextField userTextField = new JTextField();
+	private JPasswordField passwordField = new JPasswordField();
+	private JTextField nameField = new JTextField();
+	private JTextField ssnField = new JTextField();
+	private JTextField emailField = new JTextField();
+	
+	
+	private JButton insertButton = new JButton("회원가입");
+	private JButton backButton = new JButton("이전으로");
+	private JButton dupliButton = new JButton("중복확인");
+	
+	
+	private final JLabel label = new JLabel("회원가입");
+	
+
+	public InsertPage() {
+
+		frame.setPreferredSize(new Dimension(900, 600));
+		frame.setSize(900, 600);
+		frame.setTitle("로그인 페이지");
+		frame.setResizable(false);// 창 사이즈 변경 불가
+		frame.setLocationRelativeTo(null);// 모니터 중간에 창 띄우기
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		container = new JPanel();
+		container.setLocation(0, 0);
+
+		container.setSize(900, 600);
+		container.setLayout(null);
+		container.setVisible(true);// 첫 패널만 일단 보이게 해놓음
+
+
+		frame.getContentPane().add(container); // 패널로 컴포넌트를 감싸 놓음.
+		
+		//타이틀 폰트 세팅
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("NanumGothic", Font.BOLD, 28));
+		label.setBounds(262, 28, 376, 75);
+
+		//아이디 컨테이너
+		container.add(label);
+		userLabel.setBounds(274, 150, 100, 30);
+		container.add(userLabel);
+		userLabel.setFont(new Font("NanumGothic", Font.BOLD, 18));
+		
+		//비밀번호 컨테이너
+		passwordLabel.setBounds(274, 200, 100, 30);
+		container.add(passwordLabel);
+		passwordLabel.setFont(new Font("NanumGothic", Font.BOLD, 18));
+		
+		
+		//이름 컨테이너
+		container.add(label);
+		nameLabel.setBounds(274, 250, 100, 30);
+		container.add(nameLabel);
+		nameLabel.setFont(new Font("NanumGothic", Font.BOLD, 18));
+		
+		//주민번호 컨테이너
+		ssnLabel.setBounds(274, 300, 100, 30);
+		container.add(ssnLabel);
+		ssnLabel.setFont(new Font("NanumGothic", Font.BOLD, 18));
+		
+		//이메일 컨테이너
+		emailLabel.setBounds(274, 350, 100, 30);
+		container.add(emailLabel);
+		emailLabel.setFont(new Font("NanumGothic", Font.BOLD, 18));
+		
+		//사용자 텍스트 필드
+		userTextField.setBounds(378, 150, 150, 30);
+		container.add(userTextField);
+		
+		//비밀번호 텍스트 필드
+		passwordField.setBounds(378, 201, 150, 30);
+		container.add(passwordField);
+		
+		
+		//이름 텍스트 필드
+		nameField = new JTextField();
+		nameField.setBounds(378, 252, 150, 30);
+		container.add(nameField);
+		
+		ssnField = new JTextField();
+		ssnField.setBounds(378, 303, 150, 30);
+		container.add(ssnField);
+		
+		emailField = new JTextField();
+		emailField.setBounds(378, 351, 150, 30);
+		container.add(emailField);
+		
+		
+
+		//회원 가입 버튼
+		insertButton.setBounds(378, 421, 150, 40);
+		container.add(insertButton);
+		
+		//이전으로 버튼
+		backButton.setBounds(27, 28, 150, 40);
+		container.add(backButton);
+		
+		//중복확인 버튼
+		dupliButton.setBounds(558, 145, 150, 40);
+		container.add(dupliButton);
+		
+		//버튼 리슨어 
+		insertButton.addActionListener(this);
+		dupliButton.addActionListener(this);
+		backButton.addActionListener(this);
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == insertButton) {
+			new LoginPage(); // 클릭시, 다시 로그인에 돌아가 접속할 수 있게
+			frame.setVisible(false);
+		}
+		
+		if (e.getSource() == dupliButton) {
+			
+			JOptionPane.showMessageDialog(this, "중복된 아이디."); 
+			/*if(ic.getId().equals(newId)) {
+			JOptionPane.showMessageDialog(this, "이미 존재하는 아이디 입니다."); // 중복 여부 알림!!
+			
+			}else {
+				ic.setId(newId); // 값을 받아 데이터로 저장
+			}*/
+		}
+		
+		if (e.getSource() == backButton) {
+			new LoginPage(); // 클릭시, 로그인 화면 으로 돌아감
+			frame.setVisible(false);
+		}
+
+	}
+}
+ 
---
+++ src/mini/kh1/corona/view/LoginPage.java
@@ -0,0 +1,156 @@
+package mini.kh1.corona.view;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class LoginPage extends JFrame implements ActionListener {
+	
+	 
+
+	private JFrame frame = new JFrame();
+	private Container container = getContentPane();
+
+	// 레이블
+	private JLabel userLabel = new JLabel("아이디");
+	private JLabel passwordLabel = new JLabel("비밀번호");
+
+	// 필드부
+	private JTextField userTextField = new JTextField();
+	private JPasswordField passwordField = new JPasswordField();
+
+	// 버튼
+	private JButton loginButton = new JButton("로그인");
+	private JButton insertButton = new JButton("회원가입");
+	private JButton Managerbutton = new JButton("관리자");
+
+	// 체크박스
+	private JCheckBox showPassword = new JCheckBox("보이기");
+
+	// 메인 타이틀
+	private final JPanel panel = new JPanel();
+	private final JLabel label = new JLabel("코로나 백신 예약 프로그램");
+
+	public LoginPage() {
+
+		frame.setPreferredSize(new Dimension(900, 600));
+		frame.setSize(900, 600);
+		frame.setTitle("로그인 페이지");
+		frame.setResizable(false);// 창 사이즈 변경 불가
+		frame.setLocationRelativeTo(null);// 모니터 중간에 창 띄우기
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		container = new JPanel();
+
+		container.setSize(900, 600);
+		container.setLayout(null);
+		container.setVisible(true);// 첫 패널만 일단 보이게 해놓음
+
+		/*container.setSize(900, 600);
+		container.setLayout(null);
+		container.setVisible(true);*/
+
+		frame.getContentPane().add(container); // 패널로 컴포넌트를 감싸 놓음.
+
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("NanumGothic", Font.BOLD, 28));
+		label.setBounds(262, 120, 376, 75);
+
+		container.add(label);
+		
+		panel.setBounds(262, 233, 376, 199);
+
+		container.add(panel);
+		panel.setLayout(null);
+		
+		
+		userLabel.setFont(new Font("NanumGothic", Font.BOLD, 18));
+		userLabel.setBounds(0, 0, 100, 30);
+		panel.add(userLabel);
+		
+		passwordLabel.setFont(new Font("NanumGothic", Font.BOLD, 18));
+		passwordLabel.setBounds(0, 46, 100, 30);
+		panel.add(passwordLabel);
+		
+		userTextField.setBounds(115, 0, 150, 30);
+		panel.add(userTextField);
+		
+		passwordField.setBounds(115, 46, 150, 30);
+		panel.add(passwordField);
+		
+		showPassword.setBounds(297, 46, 150, 30);
+		panel.add(showPassword);
+		
+		loginButton.setBounds(0, 108, 100, 30);
+		
+		panel.add(loginButton);
+
+		// insertButton
+		insertButton.setBounds(165, 108, 100, 30);
+		panel.add(insertButton);
+		
+		//관리자 버튼
+		Managerbutton.setBounds(165, 169, 100, 30);
+		panel.add(Managerbutton);
+		
+
+		// 버튼 이벤트
+		loginButton.addActionListener(this);
+		showPassword.addActionListener(this);
+		insertButton.addActionListener(this);
+		Managerbutton.addActionListener(this);
+	}
+
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) { 
+		if (e.getSource() == loginButton) {
+			String userText;
+			String pwdText;
+			userText = userTextField.getText();
+			pwdText = passwordField.getText();
+			if (userText.equalsIgnoreCase("admin") && pwdText.equalsIgnoreCase("12345")) { // 임의 입력 // 나중에 데이터 받아옴
+				JOptionPane.showMessageDialog(this, "로그인에 성공했습니다."); // 성공 메세지!!
+				new MainMenu();
+				frame.setVisible(false);
+			} else {
+				JOptionPane.showMessageDialog(this, "아이디, 비밀번호가 일치 하지 않습니다.");
+			}
+
+		}
+		if (e.getSource() == insertButton) {
+			new InsertPage(); // 클릭시, 회원가입 창으로 넘어가게
+			frame.setVisible(false);
+		}
+		if (e.getSource() == showPassword) {
+			if (showPassword.isSelected()) { // 비밀번호 보이기 토글 세팅
+				passwordField.setEchoChar((char) 0); 
+			} else {
+				passwordField.setEchoChar('*'); // 비밀번호 감추기
+			}
+
+		}
+		
+		if (e.getSource() == Managerbutton) {
+			new ManagerView(); // 클릭시, 관리자 창으로 넘어가게
+			frame.setVisible(false);
+		}
+
+	}
+
+}
