9639f69e09c1d1bd86d2f29af59c7ccfd2d13e76
kmk9259 <kmk9259@naver.com>
2021-06-08 17:40:28 +0900

    path수ㅠ정

--- src/mini/kh1/corona/run/Run.java
+++ src/mini/kh1/corona/run/Run.java
@@ -2,6 +2,7 @@ package mini.kh1.corona.run;
 
 import mini.kh1.corona.model.vo.User;
 import mini.kh1.corona.view.LoginPage;
+import mini.kh1.corona.view.MainMenu;
 
 public class Run {
 	
@@ -9,7 +10,6 @@ public class Run {
 		
 		       new LoginPage();
 		       User u = new User("kk","1234","나", "854201-152454 ", "email@");
-		       
 		       //id + "\n" + password + "\n" + name + "\n" + ssn + "\n" + email;
 
 		    
