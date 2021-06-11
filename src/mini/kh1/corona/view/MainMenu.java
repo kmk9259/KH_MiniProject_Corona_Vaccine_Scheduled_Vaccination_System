e6e3b28f4d82a346fe6e7ac020cd604f29dbbdd0
kmk9259 <kmk9259@naver.com>
2021-06-12 03:12:02 +0900

    s

--- src/mini/kh1/corona/view/MainMenu.java
+++ src/mini/kh1/corona/view/MainMenu.java
@@ -21,7 +21,7 @@ public class MainMenu {
 	JPanel chatBotpanel = ChatBotFrame.chatPanel;
 
 	static JFrame MFrame = new JFrame();
-	static JPanel mainPanel = new JPanel(); // 메인 패널은 자주 호출될 것이므로 일단 static->직접 호출가능하도록
+	public static JPanel mainPanel = new JPanel(); // 메인 패널은 자주 호출될 것이므로 일단 static->직접 호출가능하도록
 
 	boolean isBooker = false;
 
